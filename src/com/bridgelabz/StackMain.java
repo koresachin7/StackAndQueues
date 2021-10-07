8783ae55bc0d6c7d24acefcbf057005ac198b712
koresachin7 <koresachin7@gmail.com>
2021-10-07 23:21:39 +0530

    Added : - Push Elements in to the stack

--- src/com/bridgelabz/StackMain.java
+++ src/com/bridgelabz/StackMain.java
@@ -1,7 +1,18 @@
 package com.bridgelabz;
-
+/**
+ * Purpose - To create a Stack and push elements to the stack
+ * Final sequence to be printed - 56 - 30 - 70
+ * @author - Sachin kore
+ * @version - 16.0
+ */
 public class StackMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to Stack and Queues ");
+        System.out.println("Welcome to Stack And Queues ");
+        MyStack stack = new MyStack();
+        stack.push(70);
+        stack.push(30);
+        stack.push(56);
+        stack.display();
+
     }
 }
