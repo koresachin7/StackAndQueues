58070b9d7dc23352c125ae57cedbab192643796e
koresachin7 <koresachin7@gmail.com>
2021-10-08 01:24:24 +0530

    Added : - Method To add element to queue

---
+++ src/com/bridgelabz/QueueMain.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+/**
+ * Purpose - To create a Queue and perform enqueue operation
+ * Final sequence to be printed - 56 - 30 - 70
+ * @author - Sachin Kore
+ * @version - 16.0
+ */
+
+public class QueueMain {
+    public static void main(String[] args) {
+        MyQueue queue = new MyQueue();
+        queue.enqueue(56);
+        queue.enqueue(30);
+        queue.enqueue(70);
+        queue.display();
+
+    }
+}
